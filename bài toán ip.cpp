2efef3477b5dc4bf45339e1053f8e10b487e297a
Red Chicken <136171863+levanlong2603@users.noreply.github.com>
2024-10-21 01:13:55 +0700

    Create bài toán ip.cpp

---
+++ bài toán ip.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+string s;
+bool check(int x, int y){
+    if(y-x+1>3) return 0;
+    if(s[x]=='0'){
+        if(y-x+1==1) return 1;
+        return 0;
+    }
+    if(y-x+1==1 || y-x+1==2) return 1;
+    int sum=100*(s[x]-'0')+10*(s[x+1]-'0')+s[y]-'0';
+    
+    if(0<=sum && sum<=255) return 1;
+    return 0;
+}
+int main() {
+  ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+  int t;
+  cin >> t;
+  while(t--){
+    cin >> s;
+    int n=s.length();
+    if(n<4 || n>12) {cout<<0<<'\n'; continue;}
+    int ans=0;
+    for(int i=0; i<n; ++i)
+      for(int j=i+1; j<n; ++j)
+        for(int p=j+1; p<n; ++p){
+          if(check(0,i) && check(i+1,j) && check(j+1,p) && check(p+1,n-1)) ans++;
+        }
+    cout<<ans<<'\n';
+  }
+return 0;
+}
